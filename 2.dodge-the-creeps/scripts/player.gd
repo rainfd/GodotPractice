5feab439fd12255589982bf6a87e5e0e8e5fcb1b
rainfd <rainfd@live.cn>
2025-01-01 03:24:58 +0800

    project2 completed

--- 2.dodge-the-creeps/scripts/player.gd
+++ 2.dodge-the-creeps/scripts/player.gd
@@ -8,20 +8,20 @@ var screen_size
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	position = screen_size * 0.5
+	#position = screen_size * 0.5
 	#print(screen_size, position)
 	hide()
 	
 	
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
-	if Input.is_action_just_pressed("move_right"):
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_just_pressed("move_up"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-	if Input.is_action_just_pressed("move_down"):
+	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	
 
@@ -49,3 +49,9 @@ func _on_body_entered(body: Node2D) -> void:
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
